b5611b54a3b1634fc13d5d8c6acaaa60b418806b
Charles Júnior <charlesbritoneresjr@gmail.com>
2022-06-10 17:33:55 +0800

    create Main component

---
+++ src/components/main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import image from '../../images/illustration-dashboard.png';
+
+class Main extends React.Component{
+    render(){
+        return(
+            <img src={image} alt="illustration dashboard" />
+        )
+    }
+}
+
+export default Main;
