ee67dd2cd3b9def42e01cb15e4e5991f3f5868c0
Charles Júnior <charlesbritoneresjr@gmail.com>
2022-06-10 16:47:07 +0800

    add style to Header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from '../../images/logo.svg'; 
+import HeaderCss from './HeaderCss';
 
 class Header extends React.Component{
     render(){
         return(
-            <header>
+            <HeaderCss>
                 <img src={logo} alt="logo ping coming soon page" /> 
                 <h1>We are launching <strong>soon!</strong></h1>
                 <h3>Subscribe and get notified</h3>
@@ -12,7 +13,7 @@ class Header extends React.Component{
                     <input placeholder="your email adress..." />  
                     <button type="submit">Notify Me</button>
                 </form>
-            </header>
+            </HeaderCss>
         )
     }
 }
---
+++ src/components/header/HeaderCss.js
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+const HeaderCss = styled.header`
+    padding-top: 4em;
+    padding-bottom: 4em;
+    /* background-color: aliceblue; */
+    text-align: center;
+
+    h1{
+        color: var(--neutral-color-gray);
+        margin-top: 1.4em;
+        font-size: 120%;
+
+        strong{
+            color: black;
+        }
+    }
+
+    h3{
+        margin-top: 2em;
+        margin-bottom: 2em;
+        font-size: 65%;
+    }
+
+    form > * {
+        padding: 0.6em 0.8em;
+        border-radius: 1.5em;
+        border: 0;
+        outline: 0;
+    }
+
+    form{
+        /* position: relative; */
+
+        input{            
+            border: 1px solid var(--secondary-color-pale-blue);
+            color: var(--secondary-color-pale-blue);
+            width: 100%;
+            /* width: 20em; */
+
+            @media screen and (min-width: 378px) {
+                width: 20em;
+            }
+        }
+
+        button{            
+            background-color: var(--primary-color-blue);
+            border: 1px solid var(--primary-color-blue);
+            color: #fff;
+            width: 100%;
+            margin-top: 1em; 
+            /* width: 9em; */
+
+            @media screen and (min-width: 378px) {
+                width: 9em; 
+                margin-left: .5em;
+            }
+        }    
+    }
+
+        
+`
+
+export default HeaderCss;
