0880745b86085d3faae7fb54e81d8708d0156863
Charles Júnior <charlesbritoneresjr@gmail.com>
2022-06-10 18:46:35 +0800

    add style to Main component

---
+++ src/components/main/MainCss.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const MainCss = styled.main`    
+
+    text-align: center;
+
+    img{
+        max-width:100%;
+        width: auto;
+        height: auto;
+    }    
+`;
+
+export default MainCss;
