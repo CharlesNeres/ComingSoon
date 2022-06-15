3ce0d7b68969cd90f8bbc7b9c2fd1c86637284a2
Charles Júnior <charlesbritoneresjr@gmail.com>
2022-06-15 14:53:27 +0800

    implemented email validation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,15 +3,64 @@ import logo from '../../images/logo.svg';
 import HeaderCss from './HeaderCss';
 
 class Header extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            value: '',
+            validEmail: true,
+            messageError: ''          
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.validateEmail = this.validateEmail.bind(this);
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        this.validateEmail();
+    }
+
+    handleChange(event) {    
+        this.setState({value: event.target.value});  
+    }
+
+    validateEmail(){
+        let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    
+        if(this.state.value){
+            if(this.state.value.match(mailformat)){
+                // do something if needed   
+                this.setState({
+                    validEmail: true,
+                    messageError: ''
+                });                             
+            }else{                
+                this.setState({
+                    validEmail: false,
+                    messageError: 'Please provide a valid email address'
+                });                
+            }
+        }else{            
+            this.setState({
+                validEmail: false,
+                messageError: 'Whoops! It looks like you forgot to add your email'
+            });
+        }
+    }
+
     render(){
         return(
-            <HeaderCss>
+            <HeaderCss validEmail={this.state.validEmail}>
                 <img src={logo} alt="logo ping coming soon page" /> 
                 <h1>We are launching <strong>soon!</strong></h1>
                 <h3>Subscribe and get notified</h3>
-                <form>
-                    <input placeholder="your email adress..." />  
+                <form onSubmit={this.handleSubmit}>
+                    <input placeholder="your email adress..." value={this.state.value} onChange={this.handleChange}/>  
                     <button type="submit">Notify Me</button>
+                    {this.state.messageError 
+                        ? <p id='msgError'>{this.state.messageError}</p> 
+                        : <></>
+                    }                    
                 </form>
             </HeaderCss>
         )
--- src/components/main/MainCss.js
+++ src/components/main/MainCss.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const MainCss = styled.main`    
 
     text-align: center;
-    position: static;
 
     img{
         max-width:100%;
